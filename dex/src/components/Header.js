a6439ad718cde8ebbe760b6c5765038bccce8631
Gleb Sitikov <glebsitikov4@gmail.com>
2023-03-13 21:15:56 +0100

    init

--- dex/src/components/Header.js
+++ dex/src/components/Header.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import Eth from '../eth.svg';
+import { Link } from "react-router-dom";
+
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className='leftH'>
+        <div className='headerItem'>Buy</div>
+        <Link to="/" className='link'>
+          <div className='headerItem'>Swap</div>
+        </Link>
+        <Link to="/tokens" className='link'>
+          <div className='headerItem'>Tokens</div>
+          </Link>
+        <div className='headerItem'>Stake</div>
+      </div>
+      <div className='rightH'>
+        <div className='headerItem'>
+          <img src={Eth} alt="eth" className='eth' />
+          Ethereum
+        </div>
+        <div className='connectButton'>
+          Connect
+        </div>
+      </div>
+    </header>
   )
 }
 
-export default Header
+export default Header;
--- dex/src/components/Swap.js
+++ dex/src/components/Swap.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Input, Popover, Radio, Modal, message } from "antd"
+import { ArrowDownOutlined, DownOutlined, SettingOutlined } from "@ant-design/icons"
+
 
 function Swap() {
   return (
-    <div>Swap</div>
+    <div className='tradeBox'>
+      <div className='tradeBoxHeader'>
+        <h4>Swap</h4>
+        <Popover title="Settings" trigger="click" placement='bottomRight'>
+          <SettingOutlined className="cog" />
+          </Popover>
+      </div>
+    </div>
   )
 }
 
